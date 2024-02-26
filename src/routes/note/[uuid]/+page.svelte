e54193a23d86a8ffcb3ac221e488b22e3bd2be99
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-02-26 23:41:04 +0100

    Frontend

--- src/routes/note/[uuid]/+page.svelte
+++ src/routes/note/[uuid]/+page.svelte
@@ -4,8 +4,12 @@
 
 <div class="container mx-auto">
     <div class="flex h-full">
-        <div class="m-auto">
-            <div>{data.note}</div>
+        <div class="m-auto text-xl p-3 bg-black bg-opacity-50 text-white">
+            {#if data.note}
+                <div>{data.note}</div>
+            {:else}
+                <div>Note not found</div>
+            {/if}
         </div>
     </div>
 </div>
