0f62189d7f8847c1aa15bbb440d552d5efac6538
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-02-26 23:18:03 +0100

    Add: background and minor fixes

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,13 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    vite: {
+      server: {
+        cors: {
+          origin: "http://192.145.37.253:3006/"
+        }
+      }
+    }
   },
 
   preprocess: vitePreprocess()
