eb5b58104cd8cded7add2b64e505609435dd9a78
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-02-26 23:06:00 +0100

    Add: background and minor fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script>
     import { enhance } from "$app/forms";
+    import { page } from "$app/stores";
 
     export let form;
     let success = false;
+    let link = "";
 
     $: if (form?.success) {
         success = true;
 
         setTimeout(() => {
             success = false;
+            link = $page.url.hostname + "/note/" + form?.uuid
         }, 3000);
     }
 </script>
@@ -20,7 +23,9 @@
                 <textarea name="content" class="w-full p-4 font-mono font-semibold text-lg bg-purple-800 text-white" cols="30" rows="10"></textarea>
                 <button type="submit" class="w-full p-3 font-mono font-semibold text-lg bg-purple-500">Submit</button>
             </form>
-            <span>{window.location.hostname + "/note/" + form?.uuid}</span>
+            {#if link !== ""}
+                <span>{link}</span>
+            {/if}
             {#if success}
                 <div class="container mx-auto bg-green-600 rounded-md w-full p-5">
                     <div class="flex h-full">
