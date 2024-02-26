e4452f16ee957e2bb77de178dd31eebdd7d4dbcc
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-02-26 22:03:27 +0100

    Add: tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,16 @@
     import "../app.css";
 </script>
 
-<div class="flex flex-col h-screen">
+<div class="flex flex-col justify-between h-screen">
     <div class="flex justify-center">
         <a href="/">
             <h1 class="text-3xl">Docknote</h1>
         </a>
     </div>
     
-    <slot />
+    <div class="m-auto">
+        <slot />
+    </div>
 
     <div class="flex justify-between">
         <a href="/about">About</a>
